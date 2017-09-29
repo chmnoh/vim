feabff9c69f9c688ed894f827470553d8a375bef
changmin.oh <chmn.oh@gmail.com>
2017-09-30 00:18:10 +0900

    my personal mac osx config

---
+++ myconfig.sh
@@ -0,0 +1,1 @@
+./configure --without-x --disable-gui --enable-cscope --enable-multibyte --with-features=normal --enable-fail-if-missing --disable-pythoninterp --disable-python3interp --with-tlib=ncurses
